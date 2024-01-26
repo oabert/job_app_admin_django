279ec8c3d1be6ccb1b9bd85acf5c8955b0476ced
Olena Abert <oabert@gmail.com>
2024-01-26 15:13:41 -0500

    Add admin page

--- job_app_admin/admin.py
+++ job_app_admin/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Form
 
-# Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')  # admin table view
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('date', 'occupation')
+    ordering = ('first_name',)  # it is a tuple / if '-first_name' - will order from z to a
+    readonly_fields = ('first_name',)  # it is a tuple
+
+
+admin.site.register(Form, FormAdmin)
